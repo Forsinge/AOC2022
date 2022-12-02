c644fe4381efed2323946a68be43fd040bf406f3
Forsinge <forsinge@chalmers.se>
2022-12-02 16:02:06 +0100

    Cleanups

--- 2/2.py
+++ 2/2.py
@@ -1,4 +1,4 @@
 with open('2/input.txt') as f:
-    p1, p2, matchups = [0,3,6], [1,2,3], [(ord(a)-65, ord(b)-88) for a,b in map(str.split, f.readlines())]
-    print(sum(b+1 + (p1[1-a:]+p1[:1-a])[b] for a,b in matchups))
-    print(sum(b*3 + (p2[b-1:]+p2[:b-1])[a] for a,b in matchups))
+    p1, p2, games = [0,3,6], [1,2,3], [(ord(line[0])-65, ord(line[2])-88) for line in f.readlines()]
+    print(sum(p2[b] + (p1[1-a:]+p1[:1-a])[b] for a,b in games)) # task 1
+    print(sum(p1[b] + (p2[b-1:]+p2[:b-1])[a] for a,b in games)) # task 2
