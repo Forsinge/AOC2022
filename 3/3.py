3222b1fc780a83687d962beddc23b9035f505ae7
Forsinge <forsinge@chalmers.se>
2022-12-03 19:21:40 +0100

    Cleanups

--- 3/3.py
+++ 3/3.py
@@ -1,6 +1,6 @@
 with open('3/input.txt') as f:
-    lines, letters = f.read().split('\n'), ' abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    lines, prio = f.read().split('\n'), ' abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
     halves = [(s[:len(s)//2], s[len(s)//2:]) for s in lines]
     triples = [lines[i:i+3] for i in range(0, len(lines), 3)]
-    print(sum(letters.find(min(set(a) & set(b))) for a,b in halves))  # task 1
-    print(sum(letters.find(min(set(a) & set(b) & set(c))) for a,b,c in triples)) # task 2
+    print(sum(prio.find(min(set(a) & set(b))) for a,b in halves))  # task 1
+    print(sum(prio.find(min(set(a) & set(b) & set(c))) for a,b,c in triples)) # task 2
