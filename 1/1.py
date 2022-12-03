326b25db7f377c0ee625f5383424be5aa3258ab2
Forsinge <forsinge@chalmers.se>
2022-12-03 19:19:25 +0100

    Cleanups

--- 1/1.py
+++ 1/1.py
@@ -1,3 +1,3 @@
 with open('1/input.txt') as f:
-    sums = [sum([int(item) for item in elf.splitlines()]) for elf in f.read().split('\n\n')]
-    print(sums.sort(), sums[-1], sum(sums[-3:])) # task 1 and 2
+    sums = sorted(sum(map(int, elf.split())) for elf in f.read().split('\n\n'))
+    print(sums[-1], sum(sums[-3:])) # task 1 and 2
--- 3/3.py
+++ 3/3.py
@@ -1,5 +1,6 @@
 with open('3/input.txt') as f:
     lines, letters = f.read().split('\n'), ' abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    it, halve = iter(lines), lambda str: (str[:len(str)//2], str[len(str)//2:])
-    print(sum(letters.find( min(set(a) & set(b) )) for a,b in map(halve, lines))) # task 1
-    print(sum(letters.find( min((set(line) & set(next(it)) & set(next(it))) )) for line in it)) # task 2
+    halves = [(s[:len(s)//2], s[len(s)//2:]) for s in lines]
+    triples = [lines[i:i+3] for i in range(0, len(lines), 3)]
+    print(sum(letters.find(min(set(a) & set(b))) for a,b in halves))  # task 1
+    print(sum(letters.find(min(set(a) & set(b) & set(c))) for a,b,c in triples)) # task 2
