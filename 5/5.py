ecd139b4f03a9b1846b746be04d386e56d2a8fa6
Forsinge <forsinge@chalmers.se>
2022-12-05 17:41:09 +0100

    Cleanups

--- 5/5.py
+++ 5/5.py
@@ -1,9 +1,9 @@
 with open('5/input.txt') as f:
     crates, instrs = [section.split('\n') for section in f.read().split('\n\n')]
-    stacks = [[c for c in line if c.isalpha()] for line in list(zip(*crates[::-1])) if line[0].isnumeric()]
-    copy1, copy2 = [stack[:] for stack in stacks], [stack[:] for stack in stacks]
+    stacks1 = [list(''.join(line[1:]).strip()) for line in list(zip(*crates[::-1]))[1::4]]
+    stacks2 = [stack[:] for stack in stacks1]
     for instr in instrs:
         n, fr, to = map(int, instr.split()[1::2])
-        copy1[to-1] += [copy1[fr-1].pop() for _ in range(n)]
-        copy2[to-1] += reversed([copy2[fr-1].pop() for _ in range(n)])
-    print(''.join([stack[-1] for stack in copy1]), ''.join([stack[-1] for stack in copy2])) # task 1 and 2
+        stacks1[to-1] += [stacks1[fr-1].pop() for _ in range(n)]
+        stacks2[to-1] += [stacks2[fr-1].pop() for _ in range(n)][::-1]
+    print(*[stack[-1] for stack in stacks1], ' ', *[stack[-1] for stack in stacks2]) # task 1 and 2
