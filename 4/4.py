b69f1b13e5be55370f22321251c75bb1ba6859a8
Forsinge <forsinge@chalmers.se>
2022-12-05 09:56:11 +0100

    Day 5

--- 4/4.py
+++ 4/4.py
@@ -1,4 +1,4 @@
 with open('4/input.txt') as f:
-    lines = [sorted(list(map(int, pair.split('-'))) for pair in line.split(',')) for line in f.readlines()]
-    print(sum([int(b >= d or a == c) for (a,b),(c,d) in lines])) # task 1
-    print(sum([int(b >= c) for (_,b),(c,_) in lines])) # task 2
+    pairs = [sorted(list(map(int, pair.split('-'))) for pair in line.split(',')) for line in f.readlines()]
+    print(sum([int(b >= d or a == c) for (a,b),(c,d) in pairs])) # task 1
+    print(sum([int(b >= c) for (_,b),(c,_) in pairs])) # task 2
